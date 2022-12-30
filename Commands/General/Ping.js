59b1b4d7d9446cfb7feeb7f628fccce312cf6f01
Ki77y666 <nosman666@live.co.uk>
2022-12-31 03:17:29 +1100

    Update Ping.js

--- Commands/General/Ping.js
+++ Commands/General/Ping.js
@@ -1,23 +1,12 @@
-const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, WebhookClient } = require(`discord.js`);
+const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits } = require(`discord.js`);
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator) // only allowed for admin users
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .setName(`ping`)
         .setDescription(`Pong`),
+        
     execute(interaction) {
-
-        const id = `1034893966648295475`;
-        const token = `7VHogetLs-ZaohGBIkRMbPeou4PqssmpkOz_0ULTrZUts8-JWpu1H6XE-MQUXqZVEyDI`;
-
-        const webhookClient = new WebhookClient({ id: id, token: token });
-
-        webhookClient.send({
-            content: `Webhook test`,
-            username: `some-username`,
-            avatarURL: `https://i.imgur.com/AfFp7pu.png`,
-        });
-
-        interaction.reply({ content: `success` });
+        interaction.reply({ content: `Pong`, ephemeral: true })
     },
 };
