c34f9acc05afb6fa2d63a8d1096cc04eb223d9a4
Ki77y666 <nosman666@live.co.uk>
2022-12-31 15:19:51 +1100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const logs = require("discord-logs");
 const { handleLogs } = require("./Handlers/handleLogs");
 const { loadEvents } = require("./Handlers/eventHandler");
 const { loadCommands } = require("./Handlers/commandHandler");
+const { loadButtons } = require("./Handlers/buttons");
 
 const client = new Client({
   intents: [Object.keys(GatewayIntentBits)],
@@ -20,10 +21,12 @@ logs(client, {
 });
 
 client.commands = new Collection();
+client.buttons = new Collection();
 client.config = require("./config.json");
 
 client.login(client.config.token).then(() => {
   handleLogs(client);
   loadEvents(client);
   loadCommands(client);
+  loadButtons(client);
 });
