6120698841a57330021c702a86071625202b48e1
Ki77y666 <nosman666@live.co.uk>
2023-01-22 03:25:56 +1100

    Update Clear.js

--- Commands/Moderation/Clear.js
+++ Commands/Moderation/Clear.js
@@ -2,17 +2,19 @@ const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuild
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
         .setName(`clear`)
-        .setDescription(`Clear a specific amount of messages from a channel or user.`)
+        .setDescription(`Clear a specific amount of messages from a member or channel.`)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
         .addIntegerOption(option =>
             option.setName(`amount`)
                 .setDescription(`Amount of messages to clear.`)
+                .setMinValue(1)
+                .setMaxValue(99)
                 .setRequired(true)
         )
         .addUserOption(option =>
-            option.setName(`user`)
-                .setDescription(`Select the user to clear their messages.`)
+            option.setName(`member`)
+                .setDescription(`Select a member to clear their messages.`)
                 .setRequired(false)
         ),
 
@@ -20,14 +22,13 @@ module.exports = {
         const { channel, options } = interaction;
 
         const amount = options.getInteger(`amount`);
-        const target = options.getUser(`user`);
+        const target = options.getUser(`member`);
 
         const messages = await channel.messages.fetch({
             limit: amount + 1,
         });
 
-        const res = new EmbedBuilder()
-            .setColor(0x5fb041)
+        const embed = new EmbedBuilder()
 
         if (target) {
             let i = 0;
@@ -41,13 +42,23 @@ module.exports = {
             });
 
             await channel.bulkDelete(filtered).then(messages => {
-                res.setDescription(`Succesfully deleted ${messages.size} messages from ${target}.`);
-                interaction.reply({ embeds: [res] }); // you can use ephemeral if you desire
+                embed
+                    .setColor(`Green`)
+                    .setTitle(`The mess has been cleaned!`)
+                    .setDescription(`Succesfully deleted ${messages.size} messages from ${target}.`)
+                    .setTimestamp();
+
+                interaction.reply({ embeds: [embed] });
             });
         } else {
             await channel.bulkDelete(amount, true).then(messages => {
-                res.setDescription(`Succesfully deleted ${messages.size} messages from the channel.`);
-                interaction.reply({ embeds: [res] });
+                embed
+                    .setColor(`DarkGreen`)
+                    .setTitle(`The mess has been cleaned!`)
+                    .setDescription(`Succesfully deleted ${messages.size} messages from the channel.`)
+                    .setTimestamp();
+
+                interaction.reply({ embeds: [embed] });
             });
         }
     }
