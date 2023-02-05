ded6567c036f4a8b53b1ca8d6b70f1a102e3404b
Ki77y666 <nosman666@live.co.uk>
2023-02-05 13:23:26 +1100

    Delete Models directory

--- Models/ReactionRoles.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let reactionRoles = new Schema({
-    GuildID: String,
-    roles: Array
-});
-
-module.exports = model("ReactionRoles", reactionRoles);
